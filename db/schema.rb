c608ff6bb1c2d7ec929a202ede1ff65bf762ee95
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-03-23 23:06:30 +0900

    test

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_21_104842) do
+ActiveRecord::Schema.define(version: 2023_03_22_122017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "photo_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["photo_id"], name: "index_comments_on_photo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "photo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["photo_id"], name: "index_favorites_on_photo_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "photo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["photo_id"], name: "index_likes_on_photo_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "photo_categories", force: :cascade do |t|
+    t.bigint "photo_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_photo_categories_on_category_id"
+    t.index ["photo_id"], name: "index_photo_categories_on_photo_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "file_url"
+    t.integer "iso"
+    t.decimal "shutter_speed"
+    t.decimal "f_value"
+    t.string "camera_model"
+    t.decimal "latitude"
+    t.decimal "longitude"
+    t.boolean "location_enabled"
+    t.datetime "taken_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "firebase_uid", null: false
@@ -32,4 +91,13 @@ ActiveRecord::Schema.define(version: 2023_03_21_104842) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "photos"
+  add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "photos"
+  add_foreign_key "favorites", "users"
+  add_foreign_key "likes", "photos"
+  add_foreign_key "likes", "users"
+  add_foreign_key "photo_categories", "categories"
+  add_foreign_key "photo_categories", "photos"
+  add_foreign_key "photos", "users"
 end
