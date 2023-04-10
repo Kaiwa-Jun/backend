aeca0c6eb4d0e92ac43a95d0fe34539b7fcee7d4
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-04-11 01:13:15 +0900

    位置情報のON/OFF設定の実装（バックエンド）

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -28,21 +28,28 @@ class Api::V1::PhotosController < ApplicationController
     puts "Received user_id: #{params[:user_id]}"
     user = User.find_by(firebase_uid: params[:user_id])
     puts "recieved user: #{user.inspect}"
-
+  
     uploaded_image = params[:image]
     exif_data = EXIFR::JPEG.new(uploaded_image.tempfile)
-
-    user_id = user.id # この行を修正
-    # 以降のコードは変更なし
+  
+    user_id = user.id
+    # Exifデータから取得していた位置情報をリクエストパラメータから取得するように変更
+    if params[:location_enabled] == "true"
+      latitude = params[:latitude].to_f
+      longitude = params[:longitude].to_f
+    else
+      latitude = nil
+      longitude = nil
+    end
+    location_enabled = params[:location_enabled] == "true"
+  
+    # 他のExifデータはそのまま利用
     iso = exif_data.iso_speed_ratings
     shutter_speed = exif_data.shutter_speed_value.to_s
     f_value = exif_data.f_number.to_f
     camera_model = exif_data.model
-    latitude = exif_data.gps_latitude.present? ? exif_data.gps_latitude[0] + exif_data.gps_latitude[1]/60 + exif_data.gps_latitude[2]/3600 : nil
-    longitude = exif_data.gps_longitude.present? ? exif_data.gps_longitude[0] + exif_data.gps_longitude[1]/60 + exif_data.gps_longitude[2]/3600 : nil
-    location_enabled = exif_data.gps_latitude.present? && exif_data.gps_longitude.present?
     taken_at = exif_data.date_time_original
-
+  
     photo = Photo.new(
       user_id: user_id,
       iso: iso,
@@ -54,11 +61,11 @@ class Api::V1::PhotosController < ApplicationController
       location_enabled: location_enabled,
       taken_at: taken_at
     )
-
+  
     photo.image.attach(params[:image])
-
+  
     puts "Photo before save: #{photo.inspect}"
-
+  
     if photo.save
       puts "Photo after save: #{photo.inspect}"
       image_url = url_for(photo.image) # 画像のURLを取得
