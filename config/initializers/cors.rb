58cd312699a70cfb6166833a3f387c9925e8701b
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-06-04 16:11:11 +0900

    cors設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', 'https://shotsharing-kaiwa-jun.vercel.app'
+    origins 'localhost:3001', 'https://shotsharing.vercel.app'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
