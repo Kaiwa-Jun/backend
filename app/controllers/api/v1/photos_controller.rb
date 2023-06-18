a7661df33245c1d095b0190a7a72df6eaac699dc
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-06-18 13:48:40 +0900

    検索機能実装完了

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,9 +1,13 @@
+require_relative '../../../helpers/translation_helper'
+include TranslationHelper
+
 require 'aws-sdk-s3'
 require 'exifr/jpeg'
+require "google/cloud/vision"
 
 class Api::V1::PhotosController < ApplicationController
   before_action :authenticate_user, except: [:index, :show]
-  # 現在のユーザーが投稿した写真のみを取得するアクション
+
   def user_photos
     user = User.find_by(firebase_uid: params[:user_id])
     user_photos = user.photos
@@ -11,11 +15,11 @@ class Api::V1::PhotosController < ApplicationController
     s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
 
     user_photos.each do |photo|
-      obj = s3.bucket('shotsharing').object(photo.image.key) # Rails Active Storageで保存されたキーを使用
-      photo.image_url = obj.presigned_url(:get, expires_in: 3600) # プリサインドURLを生成して格納
+      obj = s3.bucket('shotsharing').object(photo.image.key)
+      photo.image_url = obj.presigned_url(:get, expires_in: 3600)
     end
 
-    render json: user_photos.to_json(include: { image_attachment: { only: [:id, :service_name, :byte_size] }, image_blob: { only: [:key, :filename, :content_type] }, methods: [:image_url]}) # image_urlも含めてレスポンスに含める
+    render json: user_photos.to_json(include: { image_attachment: { only: [:id, :service_name, :byte_size] }, image_blob: { only: [:key, :filename, :content_type] }, methods: [:image_url]})
   end
 
   def index
@@ -34,14 +38,13 @@ class Api::V1::PhotosController < ApplicationController
     render json: @photos.as_json(include: :user)
   end
 
-
   def create
     puts "Request Parameters: #{params.inspect}"
     puts "Received user_id: #{params[:user_id]}"
     user = User.find_by(firebase_uid: params[:user_id])
 
     if user.nil?
-      return render json: { errors: "User not found", status: :not_found }
+      return render json: { errors: "User not found" }, status: :not_found
     end
 
     puts "recieved user: #{user.inspect}"
@@ -49,16 +52,20 @@ class Api::V1::PhotosController < ApplicationController
     uploaded_image = params[:image]
 
     if uploaded_image.nil?
-      return render json: { errors: "No image uploaded", status: :unprocessable_entity }
+      return render json: { errors: "No image uploaded" }, status: :unprocessable_entity
     end
 
-    uploaded_image = params[:image]
     uploaded_image_io = uploaded_image.open
-
-    exif_data = EXIFR::JPEG.new(uploaded_image_io)
+    if uploaded_image.content_type == "image/jpeg"
+      exif_data = EXIFR::JPEG.new(uploaded_image_io)
+      iso = exif_data.iso_speed_ratings
+      shutter_speed = exif_data.shutter_speed_value.to_s
+      f_value = exif_data.f_number.to_f
+      camera_model = exif_data.model
+      taken_at = exif_data.date_time_original
+    end
 
     user_id = user.id
-    # Exifデータから取得していた位置情報をリクエストパラメータから取得するように変更
     if params[:location_enabled] == "true"
       latitude = params[:latitude].to_f
       longitude = params[:longitude].to_f
@@ -68,12 +75,15 @@ class Api::V1::PhotosController < ApplicationController
     end
     location_enabled = params[:location_enabled] == "true"
 
-    # 他のExifデータはそのまま利用
-    iso = exif_data.iso_speed_ratings
-    shutter_speed = exif_data.shutter_speed_value.to_s
-    f_value = exif_data.f_number.to_f
-    camera_model = exif_data.model
-    taken_at = exif_data.date_time_original
+    vision = Google::Cloud::Vision.image_annotator
+
+    response = vision.label_detection image: params[:image].path
+    labels = response.responses.first.label_annotations
+
+    categories = labels.map do |label|
+      japanese_name = translate_to_japanese(label.description)
+      Category.find_or_create_by(name: label.description, japanese_name: japanese_name)
+    end
 
     photo = Photo.new(
       user_id: user_id,
@@ -87,18 +97,20 @@ class Api::V1::PhotosController < ApplicationController
       taken_at: taken_at
     )
 
-    uploaded_image_io.rewind
-    photo.image.attach(io: uploaded_image_io, filename: uploaded_image.original_filename, content_type: uploaded_image.content_type)
+    photo.categories = categories
 
+    uploaded_image_io.rewind
+     photo.image.attach(io: uploaded_image_io, filename: uploaded_image.original_filename, content_type: uploaded_image.content_type)
 
     puts "Photo before save: #{photo.inspect}"
 
     if photo.save
-      puts "Photo after save: #{photo.inspect}"
-      image_url = url_for(photo.image) # 画像のURLを取得
+      s3 = Aws::S3::Resource.new(region: 'ap-northeast-1')
+      obj = s3.bucket('shotsharing').object(photo.image.key)
+      image_url = obj.presigned_url(:get, expires_in: 3600)
       render json: { message: 'Image successfully uploaded', url: image_url, status: :created }
     else
-      render json: { errors: photo.errors.full_messages, status: :unprocessable_entity }
+      render json: { errors: photo.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -108,7 +120,12 @@ class Api::V1::PhotosController < ApplicationController
   end
 
   def update
-    # 特定の写真を更新する処理
+    photo = Photo.find(params[:id])
+    if photo.update(photo_params)
+      render json: { message: 'Photo successfully updated', status: :ok }
+    else
+      render json: { errors: photo.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -116,7 +133,13 @@ class Api::V1::PhotosController < ApplicationController
     if photo.destroy
       render json: { message: 'Photo successfully deleted', status: :ok }
     else
-      render json: { errors: photo.errors.full_messages, status: :unprocessable_entity }
+      render json: { errors: photo.errors.full_messages }, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:iso, :shutter_speed, :f_value, :camera_model, :latitude, :longitude, :location_enabled, :taken_at, :image)
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -4,6 +4,8 @@ class Photo < ApplicationRecord
   after_save :attach_image_url
   has_many :comments
   has_many :likes, dependent: :destroy
+  has_many :photo_categories
+  has_many :categories, through: :photo_categories
 
   def attach_image_url
     self.update_column(:file_url, self.image.service_url) unless self.file_url.present?
