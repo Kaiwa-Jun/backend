01310c0566eebd411dbfe0b7481c5e08f82ef28f
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-07-06 15:32:52 +0900

    ログインのデグレ修正（バックエンド）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_19_122843) do
+ActiveRecord::Schema.define(version: 2023_07_05_183718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
